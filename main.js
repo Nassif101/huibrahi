598f4adb868f15bebb25e74aa9082beed34b3d9b
Nassif101 <57049131+Nassif101@users.noreply.github.com>
2020-11-04 21:16:53 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function Generatepassword()
+{
+    var name = "jalal";
+    var randomNumber = Math.floor((Math.random() * 100) + 1);
+    var specialChar = "&";
+    var pass = name + randomNumber + specialChar;
+    if (confirm("Password: "+ pass)) {
+        document.getElementById("passfield").value = pass;        
+    }
+}
